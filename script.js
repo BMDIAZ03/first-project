9cf8cbc92509f72a53b62a4b3b6260131c432c99
BMDIAZ03 <BMDIAZ03@users.noreply.github.com>
2024-10-13 15:28:33 -0400

    Button Implementation
    Changing variables from VAR to CONST, the Add and Remove button functions

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 function addTodo() {
-    var  todoInput= document.getElementById("todoInpunt");
-    var todoList= document.getElementById("todoList");
-    var  todoText= todoInput.value;
+    const  todoInput= document.getElementById("todoInpunt");
+    const todoList= document.getElementById("todoList");
+    const  todoText= todoInput.value;
     
     if (todoText !== '') {
-        var listItem = document.createElement('li');
+
+        /*se creo un li de la lista de tarea*/ 
+        const listItem = document.createElement('li');
+
+        /*crear el checkbox para ponerlo como realizada */
+        const verification = document.createElement('input');
+        verification.type = 'checkbox';
+
+        const listItemText = document.createElement('span');
         listItem.textContent = todoText;
+        
+
+        /*crear el boton de eliminar*/
+        const buttonDelete = document.createElement('button');
+        buttonDelete.textContent = 'Eliminar';
+        buttonDelete.onclick = function () {
+            todoList.removeChild(listItem);
+        }
+
+        
+        listItem.appendChild(verification);
+        listItem.appendChild(listItemText);
+        listItem.appendChild(buttonDelete);
         todoList.appendChild(listItem);
+
         todoInput.value = '';
-    }  else{
+
+
+    }else{
         alert('error no se puede Guardar tareas vacias');
     }
 }
