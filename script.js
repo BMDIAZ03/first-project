eea561e347b70cf1c0c899d977fb7f8904359ccd
BMDIAZ03 <BMDIAZ03@users.noreply.github.com>
2024-10-13 02:56:55 -0400

    Insert the tasks
    Funicon was created to be able to save the tasks.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log ('hola, mundo')
+function addTodo() {
+    var  todoInput= document.getElementById("todoInpunt");
+    var todoList= document.getElementById("todoList");
+    var  todoText= todoInput.value;
+    
+    if (todoText !== '') {
+        var listItem = document.createElement('li');
+        listItem.textContent = todoText;
+        todoList.appendChild(listItem);
+        todoInput.value = '';
+    }  else{
+        alert('error no se puede Guardar tareas vacias');
+    }
+}
+
