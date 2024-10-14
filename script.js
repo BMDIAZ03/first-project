bc114439943fb68aaba87e3923fffb362a183518
BMDIAZ03 <BMDIAZ03@users.noreply.github.com>
2024-10-14 11:33:15 -0400

    Finalizing Process
    some comments and modified lines were put in

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// para cunado cargue la pantalla, me devuelva los datos guardados
 document.addEventListener('DOMContentLoaded', () =>{
-    loadTodos();
-})
+    loadlist();
+});
 
-function addTodo() {
+// funcion principal de la creacion de las tareas
+function addList() {
     const  todoInput= document.getElementById("todoInpunt");
     const todoList= document.getElementById("todoList");
     const  todoText= todoInput.value;
@@ -15,52 +17,62 @@ function addTodo() {
         /*crear el checkbox para ponerlo como realizada */
         const verification = document.createElement('input');
         verification.type = 'checkbox';
-        
+
+        /*se crea un span para separar cada tarea*/ 
         const listItemText = document.createElement('span');
         listItemText.textContent = todoText;
         
-
         /*crear el boton de eliminar*/
         const buttonDelete = document.createElement('button');
         buttonDelete.textContent = 'Eliminar';
         buttonDelete.classList.add('delete-button');
         buttonDelete.onclick = function () {
             todoList.removeChild(listItem);
-            saveTodos();
+            saveList();
         }
 
-        
+        /*se agregan todos los elementos a la lista de tareas como hijos
+        */
         listItem.appendChild(verification);
         listItem.appendChild(listItemText);
         listItem.appendChild(buttonDelete);
         todoList.appendChild(listItem);
 
-        todoInput.value = '';
-        saveTodos();
+        //se limpia el campo de text
+        todoInput.value = ''; 
+
+        //se guarda todas las tareas
+        saveList();
 
 
     }else{
+        //alert para el ususario
         alert('error no se puede Guardar tareas vacias');
     }
 }
 
-function saveTodos() {
+function saveList() {
     const todoList = document.getElementById('todoList');
     const todos = todoList.innerHTML;
     localStorage.setItem('todos',todos);
 }
 
-function loadTodos() {
+function loadlist() {
+    //recuperamos los valores guardados
     const savedTodos = localStorage.getItem('todos');
     if (savedTodos) {
         const todoList = document.getElementById('todoList');
+
+        // damos valor a el html, con los datos guardados
         todoList.innerHTML = savedTodos;
+
+        // selecion de todos los elementos y le damos funcion 
         todoList.querySelectorAll('.delete-button').forEach(button => {
             button.onclick = function() {
                 const listItem = button.parentNode;
                 todoList.removeChild(listItem);
-                saveTodos();
+                saveList();
             };
         });
     }
-}
+}º
